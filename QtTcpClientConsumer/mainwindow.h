f1e6d6eda1c413a264f7204888f519b07d908ba1
cicerojosean <cicerojosean@yahoo.com.br>
2018-06-11 08:17:23 -0300

    primeiro

---
+++ QtTcpClientConsumer/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
+  
+  void tcpConnect();
+public slots:
+  void getData();
+  void conectar();
+  void desconectar();
+  void getList();
+private:
+  Ui::MainWindow *ui;
+  QTcpSocket *socket;
+};
+
+#endif // MAINWINDOW_H
