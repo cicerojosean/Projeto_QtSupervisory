c9f9fecd12ac8fde3d9f819a92a8ac77ed1a1aa7
cicerojosean <cicerojosean@yahoo.com.br>
2018-06-27 07:35:50 -0300

    1

--- build-QtTcpClientConsumer-Desktop_Qt_5_10_1_GCC_64bit-Debug/moc_mainwindow.cpp
+++ build-QtTcpClientConsumer-Desktop_Qt_5_10_1_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../Consumidor02/mainwindow.h"
+#include "../Consumidor/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
--- build-QtTcpClientConsumer-Desktop_Qt_5_10_1_GCC_64bit-Debug/moc_plotter.cpp
+++ build-QtTcpClientConsumer-Desktop_Qt_5_10_1_GCC_64bit-Debug/moc_plotter.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../Consumidor02/plotter.h"
+#include "../Consumidor/plotter.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
